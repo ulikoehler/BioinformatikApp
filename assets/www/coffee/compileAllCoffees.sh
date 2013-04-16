e27892e0fa5256e88aa3dd750b00ecb874eeb8ab
Uli Köhler <ulikoehler@online.de>
2013-04-16 11:15:29 +0300

    Improve coffeescript call

--- assets/www/coffee/compileAllCoffees.sh
+++ assets/www/coffee/compileAllCoffees.sh
@@ -2,14 +2,4 @@
 
 # compile all coffee script files in this directory to js files
 
-# get a list of all files/folders ending with .coffee
-for f in *.coffee
-do
-  # test if its a file
-  if [ -f $f ]
-  then
-    # compile that
-    echo "Compiling $f ..."
-    coffee --compile $f
-  fi
-done
+coffee *.coffee
